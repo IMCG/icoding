2b159c8d050a42082fca710dd736e6e20d45c0dc
xuesen121415@gmail.com <374956586@qq.com>
2014-04-03 13:49:04 +0800

    random
    再说吧

---
+++ CareerUp/ArithmeticExpr.cpp
@@ -0,0 +1,23 @@
+/*http://www.careercup.com/question?id=6313757928062976
+Write a program to evaluate arithmetic/math expression
+"12 + 5*4 + 26" = 58
+"5 * 4" = 20
+"6 + 4" = 10
+"12" = 12
+*/
+//http://www.cnblogs.com/Tope/archive/2012/12/14/2812285.html
+//链接烂的看
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+int ArithmeticExpr(string &str)
+{
+	stack<int>myint;
+	stack<char>mychar;
+
+	for (auto& ch : str)
+	{
+		
+	}
+}
