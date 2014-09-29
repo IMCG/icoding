a074835be72bbbd425da2aa7cbee5856a43e8fb9
icodingc <xuesen121415@gmail.com>
2014-09-29 22:37:22 +0800

    update rename

--- SortSearch/HeapSort.cpp
+++ SortSearch/HeapSort.cpp
@@ -1,52 +1,95 @@
-//HeapSort
-//http://blog.csdn.net/morewindows/article/details/6709644
+//Head Sort & priority_queue
+//从1开始，这样才满足完全二叉树的性质
 #include <iostream>
 using namespace std;
-#define LeftChild(i) (2*(i)+1)
+const int maxn = 101;
+int R[maxn];
+//位置low上的节点调整
+void shift(int low, int high){
+	int i = low, j = 2 * i;
+	int temp = R[i];
+	while (j <= high){
+		//选取较大的儿子
+		if (j < high&&R[j] < R[j + 1])
+			j++;
+		if (temp < R[j]){
+			R[i] = R[j];
+			i = j;
+			j = 2 * i;
+		}
+		else break;
+	}
+	R[i] = temp;
+}
+void heapsort(int n){
+	//预处理成堆
+	for (int i = n / 2; i >= 1; --i)
+		shift(i, n);
+	//交换R[1]和最后节点
+	for (int i = n; i >= 2; --i){
+		swap(R[i], R[1]);
+		shift(1, i - 1);
+	}
+}
+int main(){
+	for (int i = 1; i <= 6; ++i)
+		cin >> R[i];
+	heapsort(6);
+	for (int i = 1; i <= 6; ++i)
+		cout << R[i];
+	return 0;
+}
 
-void Heap_adjust(int *a, int i, int n)
-{
-	int Child;
-	int temp;
-	for (temp = a[i]; LeftChild(i) < n;i=Child)
-	{
-		//赋值应该在这里。 Child!=n-1
-		Child = LeftChild(i);
-		if (Child!=n-1 &&a[Child + 1]>a[Child])
-			Child++;
-		if (temp < a[Child])
-			a[i] = a[Child];
-		else
-			break;
+//大项堆
+#include <iostream>
+using namespace std;
+const int maxn = 101;
+
+//堆里面节点个数
+int n;
+int R[maxn];
+//位置low上的节点调整
+void shift(int low, int high){
+	int i = low, j = 2 * i;
+	int temp = R[i];
+	while (j <= high){
+		//选取较大的儿子
+		if (j < high&&R[j] < R[j + 1])
+			j++;
+		if (temp < R[j]){
+			R[i] = R[j];
+			i = j;
+			j = 2 * i;
+		}
+		else break;
 	}
-	a[i] = temp;
+	R[i] = temp;
 }
-void HeapSort(int *a, int n)
-{
-	int i;
-	for (i = n / 2; i >= 0;i--)
-	{
-		Heap_adjust(a, i, n);
-	}
-	for (i = n - 1; i >= 0;i--)
-	{
-		swap(a[0], a[i]);
-		Heap_adjust(a, 0, i);
+//insert a new node with value v into heap
+void insertNode(int v){
+	R[++n] = v;
+	for (int k = n; k > 1; k /= 2){
+		if (R[k]>R[k / 2])
+			swap(R[k], R[k / 2]);
+		else break;
 	}
 }
-int main()
-{
-	int a[] = { 31, 41, 59, 26, 53, 58, 97 };
-	for (auto& i : a)
-	{
-		cout << i << " ";
+//delete 
+void deleteNode(){
+	swap(R[1], R[n--]);
+	shift(1, n);
+}
+void Top(){
+	cout << R[1] << "\n";
+}
+int main(){
+	n = 0;
+	int node;
+	while ((cin >> node),node){
+		insertNode(node);
 	}
-	cout << endl;
-	HeapSort(a, 7);
-	cout << "Sorted.\n";
-	for (auto& i : a)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
+	Top();
+	deleteNode();
+	Top();
+	return 0;
 }
